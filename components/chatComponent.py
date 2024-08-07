8513de03742e330bbce3ade8cfdcfe57526c0dca
colinarelliott <colinarelliott@gmail.com>
2024-08-07 15:32:06 -0400

    added photo... not displaying for some reason

--- components/chatComponent.py
+++ components/chatComponent.py
@@ -2,21 +2,27 @@ from reactpy import component, html, event
 from pathlib import Path
 from components.styleComponent import css, js
 from components.uploadComponent import handle_upload
+from components.photoComponent import Photo as photo
 
 @component
 def chat(handle_submit, set_message, message, response, clear, output_dir):
 
     return (
-        html.section(
-            css, js, 
+        html.div(
+            {"class": "bg-dark text-light ",
+             "style": {"height": "100vh", "width": "100vw", "overflow": "auto"}},
+        css, js, 
             # Section style
             html.div(
             {
-                "class": "container container-fluid border rounded p-3 m-3",
+                "class": "container container-fluid border rounded p-3 m-3 bg-dark text-light",
             },
                 # Header
-                html.h1({"style":"h1 p-2 m-2",}, "Armchair Expert Chat"),
-                html.div({"class":"row"},
+                photo("Armchair Expert Logo Dark", "armchairexpertlogodark.jpg"),
+                html.h1({"style":"h1 p-2 m-2 text-center d-flex justify-content-center",}, "Armchair Expert"),
+                html.h4({"style":"h4 p-2 m-2 text-center d-flex justify-content-center",}, "Make AI do the work of becoming an expert."),
+                html.hr(),
+                html.div({"class":"row bg-dark text-light"},
                 # upload files form
                 html.div(
                     {
@@ -24,24 +30,24 @@ def chat(handle_submit, set_message, message, response, clear, output_dir):
                         },
                     html.form(
                         {
-                        "class":"form-control p-2 m-2",
+                        "class":"form-control p-2 m-2 bg-dark text-light",
                         "on_submit": handle_submit,
                         "style": {"display": "inline-grid"},
                         "enctype": "multipart/form-data",
                         },
                         html.label(
                             { "for": "formFile", "class": "form-label" },
-                            "Upload Files"
+                            "Upload File(s) (WIP)"
                         ),
                         html.input(
-                            { "class": "form-control", "type": "file", "id":"formFile", "multiple": "true", "value": "", "on_change": handle_upload },
+                            { "class": "form-control bg-dark text-light", "type": "file", "id":"formFile", "multiple": "true", "value": "", "on_change": handle_upload },
                         ),
                         html.button({
-                            "class": "form-control btn btn-primary d-grid gap-2 d-md-flex",
+                            "class": "form-control btn btn-info d-grid gap-2 d-md-flex",
                             "type": "submit",
                             "for": "formFile",
                             "on_click":handle_upload,
-                            "disabled":"true"}, "Upload",
+                            "disabled":"true"}, "Upload to AI",
                             
                         ),
                     ),
@@ -51,19 +57,19 @@ def chat(handle_submit, set_message, message, response, clear, output_dir):
                     {
                      "class": "mb-3 col-8",
                      },
-                html.h4({"style":"h4 p-2 m-2",},"Loaded file(s):\n"),
+                html.h5({"style":"h5 p-2 m-2",},"Loaded file(s):\n"),
                 html.ul(
                     {
-                        "class": "list-group p-2 m-2",
+                        "class": "list-group p-2 m-2 bg-dark text-light",
                     },
                     [html.li(
-                        {"class": "list-group-item"},
+                        {"class": "list-group-item bg-dark text-light"},
                         str(file)) for file in list(Path(output_dir).glob("**/*"))]
                 ),),),
                 # Form
                 html.form(
                     {
-                        "class":"form-control p-2 m-2",
+                        "class":"form-control p-2 m-2 bg-dark text-light",
                         "on_submit": handle_submit,
                         "style": {"display": "inline-grid"}},
                     # Input Text Box
@@ -73,7 +79,7 @@ def chat(handle_submit, set_message, message, response, clear, output_dir):
                             "placeholder": "Your message...",
                             "value": message,
                             "on_change": lambda event: set_message(event["target"]["value"]),
-                            "class": "form-control form-input",
+                            "class": "form-control form-input bg-dark text-light",
                         }
                     ),
                     # Submit Button
@@ -92,13 +98,13 @@ def chat(handle_submit, set_message, message, response, clear, output_dir):
             ),
             # Response Box
             html.p({
-                "class": "p-3 mb-2 bg-light text-dark",
+                "class": "p-3 mb-2 bg-dark text-light",
             },{response
             }),
             # Footer
             html.footer(
                 {
-                    "class": "footer p-3 m-2 bg-light text-dark text-center",
+                    "class": "footer p-3 m-2 bg-dark text-muted text-center",
                 },
                 html.label({
                     "class": "unselectable label",
